d98130423c9cedf6867c25ee7dd2d8cfdf4b5f88
KimMH853 <kimmh853@gmail.com>
2023-03-26 22:39:34 +0900

    pagination

---
+++ src/routes/board/+page.js
@@ -0,0 +1,11 @@
+export const load = async ({ fetch }) => {
+  //   const productRes = await fetch("https://dummyjson.com/products?limit=10");
+  //   const productData = await productRes.json();
+  //   const products = productData.products;
+  //   return {
+  //     products: products,
+  //   };
+  const testRes = await fetch("http://localhost:3000/board");
+  const testData = await testRes.json();
+  return { testData };
+};
---
+++ src/routes/board/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let data;
+
+  const { testData } = data;
+  console.log(data);
+</script>
+
+<svelte:head>
+  <title>Board</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  {#each testData as item}
+    <p>{item.id}</p>
+    <p>{item.title}</p>
+    <p>{item.create_date}</p>
+  {/each}
+</div>
+
+<style>
+  .text-column {
+    width: 80%;
+    display: flex;
+  }
+</style>
